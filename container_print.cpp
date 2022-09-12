3b54074a0b07abbdf57edc67eb659a3598d26daa
axios <jamespenabas8@gmail.com>
2022-09-12 06:13:39 +0100

    initial

---
+++ container_print.cpp
@@ -0,0 +1,21 @@
+#include <ostream>
+#include <iterator>
+#include <vector>
+
+/*
+* Printing the content of a container with elements 
+* supporting the << operator
+*/
+
+using std::ostream_iterator;
+using std::cout;
+using std::vector;
+
+
+int main(void) {
+	vector<int> ivec{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	ostream_iterator<int> out_it(cout, ", ");
+	// Print out the content of the vector from beginning to end
+	// upon each aassinment to the out_it, the underlying stream is called
+	std::copy(ivec.cbegin(), ivec.cend(), out_it);
+}
